28d25d651b30df4d09a6649ab6a0e140655dc455
HernanVerges <hernansyc@gmail.com>
2025-05-04 23:36:16 -0300

    Correccion events.js

--- server/routes/events.js
+++ server/routes/events.js
@@ -32,7 +32,7 @@ router.get('/', async (req, res) => {
 
 router.post('/', upload.single('image'), async (req, res) => {
   try {
-    const { title, description, date, time, location, category } = req.body;
+    const { title, description, date, time, direction, city, province, price, category } = req.body;
 
     const event = new Event({
       title,

d9887f19aafca8cd7d5e806e33159299a83e13e3
HernanVerges <hernansyc@gmail.com>
2025-05-14 21:33:08 -0300

    Hamburguer menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,12 +5,13 @@ import AuthModal from './AuthModal';
 
 function Header() {
   const [showAuthModal, setShowAuthModal] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false); 
 
   const openAuthModal = () => setShowAuthModal(true);
   const closeAuthModal = () => setShowAuthModal(false);
 
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
+  const toggleMenu = () => setMenuOpen(!menuOpen);
 
   useEffect(() => {
     const token = localStorage.getItem('token');
@@ -20,10 +21,7 @@ function Header() {
   }, []);
 
   return (
-
-    
     <div className='header-container'>
-      
       <div className='logo-container'>
         <img
           src='/images/planazo-logo.png'
@@ -32,17 +30,22 @@ function Header() {
         />
       </div>
 
-      <nav className='nav-menu'>
-        <Link className='nav-Link' to='/'>Home</Link>
-        <Link className='nav-Link' to='#'>Eventos</Link>
-        <Link className='nav-Link' to='#'>Mapa</Link>
+      <button className='hamburger' onClick={toggleMenu}>
+        ☰
+      </button>
+
+      <nav className={`nav-menu ${menuOpen ? 'open' : ''}`}>
+        <Link className='nav-Link' to='/' onClick={() => setMenuOpen(false)}>Home</Link>
+        <Link className='nav-Link' to='#' onClick={() => setMenuOpen(false)}>Eventos</Link>
+        <Link className='nav-Link' to='#' onClick={() => setMenuOpen(false)}>Mapa</Link>
 
         {isLoggedIn ? (
-          <Link className='nav-Link' to='/profile'>Perfil</Link>
-          ) : (
-          <Link className='nav-Link' onClick={openAuthModal}>Perfil</Link>
-          )}
-
+          <Link className='nav-Link' to='/profile' onClick={() => setMenuOpen(false)}>Perfil</Link>
+        ) : (
+          <Link className='nav-Link' onClick={() => { openAuthModal(); setMenuOpen(false); }}>
+            Perfil
+          </Link>
+        )}
       </nav>
 
       <div className='search-bar'>
